11731b4a6864b6d5a333052476a613a3093c4c68
mcudy35 <utkucansengun@gmail.com>
2023-04-16 22:10:48 +0300

    .

--- src/test/java/pages/Departments_POM.java
+++ src/test/java/pages/Departments_POM.java
@@ -17,6 +17,20 @@ public class Departments_POM extends MyMethods {
     @FindBy(xpath = "(//span[normalize-space()='Departments'])[1]")
     private WebElement departmentsButton;
 
+    @FindBy(css = "svg[class='svg-inline--fa fa-plus']")
+    private WebElement addSchoolDepartmentButton;
+
+    @FindBy(xpath = "//input[@data-placeholder= 'Name']")
+    private WebElement nameSpace;
+
+    @FindBy(xpath = "//input[@data-placeholder= 'Code']")
+    private WebElement codeSpace;
+
+    @FindBy(xpath = "//span[text()='Save']")
+    private WebElement saveButton;
+
+    @FindBy(xpath = "//div[contains(text(),'successfully')]")
+    private WebElement successMessage;
 
     public WebElement getHumanResourcesButton() {
         return humanResourcesButton;
@@ -24,5 +38,25 @@ public class Departments_POM extends MyMethods {
 
     public WebElement getDepartmentsButton() {
         return departmentsButton;
+    }
+
+    public WebElement getAddSchoolDepartmentButton() {
+        return addSchoolDepartmentButton;
+    }
+
+    public WebElement getNameSpace() {
+        return nameSpace;
+    }
+
+    public WebElement getCodeSpace() {
+        return codeSpace;
+    }
+
+    public WebElement getNameSpace() {
+        return nameSpace;
+    }
+
+    public WebElement getSuccessMessage() {
+        return successMessage;
     }
 }
